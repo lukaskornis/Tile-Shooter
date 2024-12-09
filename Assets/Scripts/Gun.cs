f8772bfff9206123a8c2a1aafd752f802362b02e
Lukas <39262485+lukaskornis@users.noreply.github.com>
2024-12-09 18:41:13 +0200

    Ammo

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -3,19 +3,46 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
 	public GameObject bulletPrefab;
+
+	public int maxAmmo = 10;
+	public int currentAmmo;
+
+	public bool isAutomatic = true;
 	public float fireInterval = 0.5f;
+	public float fireCooldown;
+
+	AudioSource audioSource;
+
+	void Start()
+	{
+		audioSource = GetComponent<AudioSource>();
+		currentAmmo = maxAmmo;
+	}
 
 	void Update()
 	{
-		if (Input.GetButtonDown("Fire1"))
+		if (!isAutomatic &&Input.GetButtonDown("Fire1"))
 		{
 			Shoot();
 		}
+
+		if (isAutomatic &&Input.GetButton("Fire1") && fireCooldown <= 0)
+		{
+			Shoot();
+			fireCooldown = fireInterval;
+		}
+
+		fireCooldown -= Time.deltaTime;
 	}
 
 	void Shoot()
 	{
+		if(currentAmmo <= 0) return;
+		currentAmmo--;
+
 		GameObject obj = Instantiate( bulletPrefab, transform.position, transform.rotation );
 		obj.GetComponent<Bullet>().owner = gameObject;
+
+		audioSource.PlayOneShot(audioSource.clip);
 	}
 }
