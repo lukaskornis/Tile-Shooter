3295baac2ba4c487d2f3c32c0603071efe8b7083
Lukas <39262485+lukaskornis@users.noreply.github.com>
2024-12-15 12:32:00 +0200

    Flip Sides

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -16,16 +16,26 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
+        // Ground check
         RaycastHit2D hit = Physics2D.Raycast( transform.position, Vector2.down,groundDistance,groundLayer);
         grounded = hit.collider != null;
 
+        // Jump
         if (Input.GetButtonDown("Jump") && grounded)
         {
             Jump();
         }
 
+        // Movement
         float h = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(h * moveSpeed, rb.velocity.y);
+
+        //Flip sprite
+        if (h != 0)
+        {
+            float angle = h > 0 ? 0 : 180;
+            transform.eulerAngles = new Vector3(0, angle, 0);
+        }
     }
 
     void Jump()
